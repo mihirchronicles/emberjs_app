28e863c1d659a77b0ba3843847a232886de799c5
MIHIR PATEL <mihirptl89@gmail.com>
2016-04-06 13:58:28 -0500

    merge libraries/ new and edit into form.hbs

--- app/routes/libraries/edit.js
+++ app/routes/libraries/edit.js
@@ -6,6 +6,17 @@ export default Ember.Route.extend({
     return this.store.findRecord('library', params.library_id);
   },
 
+  setupController(controller, model) {
+    this._super(controller, model);
+
+    controller.set('title', 'Edit library');
+    controller.set('buttonLabel', 'Save changes');
+  },
+
+  renderTemplate() {
+    this.render('libraries/form');
+  },
+
   actions: {
 
     saveLibrary(newLibrary) {
@@ -13,7 +24,6 @@ export default Ember.Route.extend({
     },
 
     willTransition(transition) {
-
       let model = this.controller.get('model');
 
       if (model.get('hasDirtyAttributes')) {
@@ -28,44 +38,3 @@ export default Ember.Route.extend({
     }
   }
 });
-
-// import Ember from 'ember';
-
-// export default Ember.Route.extend({
-
-//   model(params) {
-//     return this.store.findRecord('library', params.library_id);
-//   },
-
-//   setupController(controller, model) {
-//     this._super(controller, model);
-
-//     controller.set('title', 'Edit library');
-//     controller.set('buttonLabel', 'Save changes');
-//   },
-
-//   renderTemplate() {
-//     this.render('libraries/form');
-//   },
-
-//   actions: {
-
-//     saveLibrary(newLibrary) {
-//       newLibrary.save().then(() => this.transitionTo('libraries'));
-//     },
-
-//     willTransition(transition) {
-//       let model = this.controller.get('model');
-
-//       if (model.get('hasDirtyAttributes')) {
-//         let confirmation = confirm("Your changes haven't saved yet. Would you like to leave this form?");
-
-//         if (confirmation) {
-//           model.rollbackAttributes();
-//         } else {
-//           transition.abort();
-//         }
-//       }
-//     }
-//   }
-// });
